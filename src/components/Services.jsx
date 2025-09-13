337796f385e1fdea5ad99c4ede4dd98db2f954bb
pedrolamborne <marildankula@gmail.com>
2025-09-13 14:06:18 +0100

    Create Services.jsx

---
+++ src/components/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Services() {
+  return (
+    <section id="services" className="services">
+      <h2>Serviços</h2>
+      <ul>
+        <li>Desenvolvimento Web</li>
+        <li>Aplicações Mobile</li>
+        <li>Consultoria Técnica</li>
+      </ul>
+    </section>
+  );
+}
