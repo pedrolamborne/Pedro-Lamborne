72559722a69be04de8b7a91b4b883aee2b0788d3
pedrolamborne <marildankula@gmail.com>
2025-09-13 14:05:02 +0100

    Create About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <section id="about" className="about">
+      <h2>Sobre mim</h2>
+      <p>Sou desenvolvedor fullstack com experiência em projetos web e mobile. Crio aplicações modernas e seguras, com backend confiável e frontend envolvente.</p>
+    </section>
+  );
+}
