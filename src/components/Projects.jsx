b3c18465d575d13c6ee3e74841bbd26e5a13adba
pedrolamborne <marildankula@gmail.com>
2025-09-13 14:05:41 +0100

    Create Projects.jsx

---
+++ src/components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Projects() {
+  return (
+    <section id="projects" className="projects">
+      <h2>Projetos</h2>
+      <div className="project-list">
+        <div className="project-card">Projeto 1</div>
+        <div className="project-card">Projeto 2</div>
+        <div className="project-card">Projeto 3</div>
+      </div>
+    </section>
+  );
+}
